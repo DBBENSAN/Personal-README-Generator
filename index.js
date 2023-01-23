085685b870e8a13595f18bb78ce9132ce42931a3
DBENSAN <dftb97@gmail.com>
2023-01-22 20:45:25 -0800

    boilerplate w/ gitignore

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+//return the anwers/designated headers with key:value pairs so that you can iterate through the objects and append them to the file name :)
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+}
+
+// Function call to initialize app
+init();
