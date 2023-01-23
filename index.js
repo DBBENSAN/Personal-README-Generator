53a1bf9109b7080b2705f863e45ac52120db0245
DBENSAN <dftb97@gmail.com>
2023-01-23 00:39:07 -0800

    adds questions array

--- index.js
+++ index.js
@@ -4,9 +4,45 @@ const fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please describe your project."
+    },
+    {
+        type: "input",
+        name: "installation instructions",
+        message: "How do you install your app?"
+    },
+    {
+        type: "input",
+        name: "usage information",
+        message: "How do you use your app?"
+    },
+    {
+        type: "input",
+        name: "contribution guidelines",
+        message: "Please provide the names of the other contributors"
+    },
+    {
+        type: "input",
+        name: "test instructions",
+        message: "Please provide any testing instructions"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which of these licenses would you like to use?',
+        choices: ['Apache License 2.0', 'BSD 3-Clause "New" or "Revised" license','BSD 2-Clause "Simplified" or "FreeBSD" license','GNU General Public License (GPL)','GNU Library or "Lesser" General Public License (LGPL)','MIT license','Mozilla Public License 2.0 license','Common Development and Distribution License','Eclipse Public License version 2.0', 'other'],
+    },
+];
 
-//return the anwers/designated headers with key:value pairs so that you can iterate through the objects and append them to the file name :)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
