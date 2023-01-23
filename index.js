72033bb33cecda4c61af3f60aaa8b7e70b37132d
DBENSAN <dftb97@gmail.com>
2023-01-23 00:55:07 -0800

    ths commit adds the init to call the questions prompt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+
 
 
 // TODO: Create an array of questions for user input
@@ -52,8 +52,7 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.appendFile(`${fileName}.md`, data, 
-    (err) => err ? console.error(err) : console.log(`${fileName}.md generated === SUCCESS ===`))
+    
 }
 
 // Create a function to initialize app
