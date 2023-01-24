faeac1c933bd1d909b0a100c8538499f635a5ea6
DBENSAN <dftb97@gmail.com>
2023-01-23 21:21:39 -0800

    adds license redering

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const questions = [
     {
         type: "input",
         name: "description",
-        message: "Please describe your project."
+        message: "Please describe your project:"
     },
     {
         type: "input",
-        name: "installation",
+        name: "install",
         message: "How do you install your app?"
     },
     {
@@ -29,12 +29,12 @@ const questions = [
     {
         type: "input",
         name: "contribution",
-        message: "Please provide the names of the other contributors"
+        message: "Please provide the names of the other contributors:"
     },
     {
         type: "input",
         name: "test",
-        message: "Please provide any testing instructions"
+        message: "Please provide any testing instructions:"
     },
     {
         type: "input",
@@ -42,23 +42,35 @@ const questions = [
         message: "What email can users direct their questions to?"
     },
     {
+        type: "input",
+        name: "username",
+        message: "Please provide a Github Username:"
+    },
+    {
         type: 'list',
         name: 'license',
         message: 'Which of these licenses would you like to use?',
-        choices: ['Apache License 2.0', 'BSD 3-Clause "New" or "Revised" license','BSD 2-Clause "Simplified" or "FreeBSD" license','GNU General Public License (GPL)','GNU Library or "Lesser" General Public License (LGPL)','MIT license','Mozilla Public License 2.0 license','Common Development and Distribution License','Eclipse Public License version 2.0', 'other'],
+        choices: ['Apache License 2.0', 'BSD 3-Clause','BSD 2-Clause','GNU General Public License (GPL)','GNU Library or "Lesser" General Public License (LGPL)','MIT license','Mozilla Public License 2.0 license','Common Development and Distribution License','Eclipse Public License version 2.0', 'other'],
+    },
+    {
+        type: "input",
+        name: "fileName",
+        message: "Name the project file:"
     },
 ];
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    
+function writeToFile(data) {
+    const writeName = data.fileName
+    fs.appendFile(`${writeName}.md`, data, (err) => (err) ? console.log(err) : console.log("Success"))
 }
 
 // Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then((answers) => {
+        console.log(answers)
         writeToFile(answers)
     })
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,22 +1,96 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  if (license === 'other') {
+    return ''
+  } else {
+    let badge = license.replaceAll(" ", "%20")
+    return `![${badge}](https://img.shields.io/badge/license-'+${badge}+'-blue)`
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'other') {
+    return ''
+  } else if (license === 'Apache License 2.0') {
+    return `[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === 'BSD 3-Clause') {
+    return `[BSD 3-Clause](https://opensource.org/licenses/BSD-3-Clause)`
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  } else if (license === 'BSD 2-Clause') {
+    return `[BSD 2-Clause](https://opensource.org/licenses/BSD-2-Clause)`
+  }
+  else if (license === 'GNU General Public License (GPL)') {
+    return `[GPL](https://opensource.org/licenses/gpl-license)`
+  }
+  else if (license === 'GNU Library or "Lesser" General Public License (LGPL)') {
+    return `[LGPL](https://opensource.org/licenses/LGPL-2.0)`
+  }
+  else if (license === 'MIT license') {
+    return `[MIT](https://opensource.org/licenses/MIT)`
+  }
+  else if (license === 'Mozilla Public License 2.0 license') {
+    return `[Mozilla Public License](https://opensource.org/licenses/MPL-2.0)`
+  }
+  else if (license === 'Common Development and Distribution License') {
+    return `[Common Development and Distribution License](https://opensource.org/licenses/CDDL-1.0)`
+  }
+  else (license === 'Eclipse Public License version 2.0') {
+    return `[Eclipse Public License](https://opensource.org/licenses/EPL-2.0)`
+  }
 }
 
-module.exports = generateMarkdown;
+
+
+  // TODO: Create a function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+    return `Read more about ${license} here:`
+ }
+
+  // TODO: Create a function to generate markdown for README
+  function generateMarkdown(data) {
+    return `
+  # ${data.title}
+  ## Badges
+  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  * [License](#license)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [How to Contribute](#how-to-contribute)
+  * [Tests](#tests)
+  * [Questions?](#questions)
+
+
+  ## License
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## How to Contribute
+  [Contributor Covenant](https://www.contributor-covenant.org/)  
+  ${data.contribution}
+  ## Tests
+  ${data.testing}
+
+  ## Questions?
+  ### Reach me here: 
+  [${data.username}](https://github.com/${data.username})  
+  ${data.email}
+
+`;
+  }
+
+  module.exports = generateMarkdown;
